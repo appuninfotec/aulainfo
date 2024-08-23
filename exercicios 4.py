1af69f586f29e6adbfc7a7edff5930a4553e2950
appuninfotec <160513181+appuninfotec@users.noreply.github.com>
2024-08-23 11:13:25 -0300

    Add files via upload

---
+++ exercicios 4.py
@@ -0,0 +1,15 @@
+
+#4) Soma de Números
+#Desenvolva um programa que receba dez números do usuário e imprima a soma de todos os números digitados.
+
+
+soma = 0  # Inicia a soma em 0
+
+# Loop que executa 10 vezes
+for i in range(10):
+    # Solicita ao usuário que digite um número e converte para inteiro
+    numero = int(input("Digite um número: "))
+    soma += numero  # Adiciona o número digitado à soma
+
+# Exibe o resultado da soma de todos os números digitados
+print(f"A soma dos números digitados é: {soma}")
